5b171792f85526cf4c403ff40a538e6244c10cfc
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-26 14:12:40 +0300

    initial commit

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+//const titleCased = () => tutorials.map(tutorial => tutorial[0].toUpperCase() + tutorial.slice(1))
+
+function titleCased() {
+  return tutorials.map(title => {
+    return title.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
